5fc45122e3761504ce92f95ca2dc805ae18e98da
Axel LAPLACE <axellaplace1709@gmail.com>
2025-06-20 14:14:57 +0200

    T1 : project config

---
+++ tailwind.config.ts
@@ -0,0 +1,9 @@
+import { Config } from "tailwind-merge";
+
+const config: Config = {
+  content: [
+    "./pages/**/*.{,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+};
