cd14493112590e72054262616769c424774772f3
Axel LAPLACE <axellaplace1709@gmail.com>
2025-06-24 11:43:00 +0200

    T1 : project setup + tanstack query added

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,15 @@
-import { Config } from "tailwind-merge";
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    "./pages/**/*.{,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
 };
+
+export default config;
