2d9c5a6b65a8bcc5ab8da2a1c70aeef76400d77b
Axel LAPLACE <axellaplace1709@gmail.com>
2025-09-09 11:19:44 +0200

    code block added

--- app/admin/courses/[courseId]/lessons/[lessonId]/edit/content/InitializedMDXEditor.tsx
+++ app/admin/courses/[courseId]/lessons/[lessonId]/edit/content/InitializedMDXEditor.tsx
@@ -2,15 +2,50 @@
 // InitializedMDXEditor.tsx
 import {
   BoldItalicUnderlineToggles,
+  ChangeCodeMirrorLanguage,
+  codeBlockPlugin,
+  codeMirrorPlugin,
+  ConditionalContents,
   headingsPlugin,
+  InsertCodeBlock,
+  InsertSandpack,
   markdownShortcutPlugin,
   MDXEditor,
   quotePlugin,
+  SandpackConfig,
+  sandpackPlugin,
+  ShowSandpackInfo,
   toolbarPlugin,
   UndoRedo,
   type MDXEditorProps,
 } from "@mdxeditor/editor";
 import styles from "./mdx-editor.module.css";
+
+const defaultSnippetContent = `
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2>
+    </div>
+  );
+}`.trim();
+
+const simpleSandpackConfig: SandpackConfig = {
+  defaultPreset: "react",
+  presets: [
+    {
+      label: "React",
+      name: "react",
+      meta: "live react",
+      sandpackTemplate: "react",
+      sandpackTheme: "light",
+      snippetFileName: "/App.js",
+      snippetLanguage: "jsx",
+      initialSnippetContent: defaultSnippetContent,
+    },
+  ],
+};
 
 // Only import this to the next file
 export default function InitializedMDXEditor({ ...props }: MDXEditorProps) {
@@ -22,11 +57,36 @@ export default function InitializedMDXEditor({ ...props }: MDXEditorProps) {
         headingsPlugin(),
         quotePlugin(),
         markdownShortcutPlugin(),
+        codeBlockPlugin({ defaultCodeBlockLanguage: "js" }),
+        sandpackPlugin({ sandpackConfig: simpleSandpackConfig }),
+        codeMirrorPlugin({
+          codeBlockLanguages: { js: "JavaScript", css: "CSS" },
+        }),
         toolbarPlugin({
           toolbarContents: () => (
             <div className="flex gap-2">
               <UndoRedo />
               <BoldItalicUnderlineToggles />
+              <ConditionalContents
+                options={[
+                  {
+                    when: (editor) => editor?.editorType === "codeblock",
+                    contents: () => <ChangeCodeMirrorLanguage />,
+                  },
+                  {
+                    when: (editor) => editor?.editorType === "sandpack",
+                    contents: () => <ShowSandpackInfo />,
+                  },
+                  {
+                    fallback: () => (
+                      <>
+                        <InsertCodeBlock />
+                        <InsertSandpack />
+                      </>
+                    ),
+                  },
+                ]}
+              />
             </div>
           ),
         }),
